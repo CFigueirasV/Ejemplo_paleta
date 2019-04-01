c8709a9c79bcafab1884c5eaff2a640db87744a0
cfigueirasvalverde <cfigueirasvalverde@nautilus26.danielcastelao.org>
2019-04-01 12:18:13 +0200

    ejemplo_paleta

---
+++ Ejemplo_paleta/Ejemplo_paleta/src/ejemplo_paleta/Ejemplo_paleta.java
@@ -0,0 +1,14 @@
+package ejemplo_paleta;
+
+
+public class Ejemplo_paleta {
+
+    
+    public static void main(String[] args) {
+        
+        Ventana ven = new Ventana();
+        ven.setVisible(true);
+        
+    }
+    
+}
